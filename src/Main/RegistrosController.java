04a320d2f650679ea29f4ed9b1001b83c57f9cf3
Claudia Asanza <clasrome@espol.edu.ec>
2020-07-12 02:40:46 -0500

    ids Ventana registro, guardar datosPacientes

--- src/Main/RegistrosController.java
+++ src/Main/RegistrosController.java
@@ -7,7 +7,13 @@ package Main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,10 +21,68 @@ import javafx.fxml.Initializable;
  * @author USUARIO
  */
 public class RegistrosController implements Initializable {
+   @FXML
+    private ResourceBundle resources;
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField nombrePaciente;
+
+    @FXML
+    private TextField apellidoPaciente;
+
+    @FXML
+    private TextField edadPaciente;
+
+    @FXML
+    private ChoiceBox<?> genero;
+
+    @FXML
+    private ChoiceBox<?> sintoma;
+
+    @FXML
+    private Button guardarPaciente;
+
+    @FXML
+    private TextField nombreDoctor;
+
+    @FXML
+    private TextField apellidoDoctor;
+
+    @FXML
+    private TextField especialidad;
+
+    @FXML
+    private Button guardarDoctor;
+
+    @FXML
+    private TextField numeroPuesto;
+
+    @FXML
+    private TextField medicoPuesto;
+
+    @FXML
+    private ComboBox<?> eliminarPuesto;
+
+    @FXML
+    private Button guardarPuesto;
+
+    @FXML
+    void guardarDoctor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarPaciente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarPuesto(ActionEvent event) {
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
