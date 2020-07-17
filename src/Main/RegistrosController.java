defe40046035381b615c39d2016266ede353842e
Claudia Asanza <clasrome@espol.edu.ec>
2020-07-16 19:19:41 -0500

    FXMLREGISTROS, set data to comobox, metodos para guardar informacion, (revisar persistencia de datos)

--- src/Main/RegistrosController.java
+++ src/Main/RegistrosController.java
@@ -6,7 +6,12 @@
 package Main;
 
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -29,27 +34,60 @@ public class RegistrosController implements Initializable {
 
     //FUNCIONALIDAD COMBOBOX
     ObservableList<String> Lgenero;
+    ObservableList<String> LEspecialidad;
+    ObservableList<String> LSintomas = leerArchivo("sintomas.txt");
+    ObservableList<String> LMedico = leerArchivo("medico.txt");  //AGREGAR INFORMACION
+    ObservableList<String> LPuesto = leerArchivo("puesto.txt"); // AGREGAR INFORMACION
     @FXML
     private ComboBox<String> cmbGenero;
-
-    /**
-     * Method for set Itemns to cmbGenero.
-     */
-    private void loadData() {
-        cmbGenero.getItems().setAll(new String[]{"MASCULINO", "FEMENINO"});
-    }
     @FXML
-    private ComboBox<String> cmbPuesto;
-
+    private ComboBox<String> cmbEspecialidad;
     @FXML
     private ComboBox<String> cmbSintomas;
     @FXML
     private ComboBox<String> cmbMedicoresponsable;
     @FXML
-    private ComboBox<String> cmbEspecialidad;
+    private ComboBox<String> cmbPuesto;
 
-    
-    
+    /**
+     * Method for set Itemns to combobox.
+     */
+    private void loadData() {
+        cmbGenero.getItems().setAll(new String[]{"MASCULINO", "FEMENINO"});
+        cmbEspecialidad.getItems().setAll(new String[]{"Medicina General", "Alergología", "Cardiología", "Angiología", "Cirugía General", "Dermatología", "Endocrinología", "Ecografía", "Hematología"});
+        cmbSintomas.getItems().setAll(LSintomas);
+        cmbMedicoresponsable.getItems().setAll(LMedico);
+        cmbPuesto.getItems().setAll(LPuesto);
+    }
+
+    /**
+     * Method for read data.
+     *
+     * @return ObsevableList<String>
+     */
+    private ObservableList<String> leerArchivo(String archivo) {
+        ObservableList<String> lista = FXCollections.observableArrayList();
+        try ( BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] data = linea.split("\\|");
+                if (archivo.equals("sintomas.txt") || archivo.equals("puesto.txt")) { //Puesto y sintoma tendra la informacion requerida en la misma posicion
+                    String sintoma_puesto = data[0];
+                    lista.add(sintoma_puesto);
+                }
+                if (archivo.equals("medico.txt")) {
+                    String NombreAp = data[0] + data[1];
+                    lista.add(NombreAp);
+
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Archivo no encontrado");
+        }
+        return lista;
+
+    }
+
     //TEXT FIELDS
     @FXML
     private TextField txtNumeroPuesto;
@@ -80,6 +118,26 @@ public class RegistrosController implements Initializable {
      */
     @FXML
     void guardarDoctor(ActionEvent event) {
+        if (event.getTarget() == btnGuardarDoctor) {
+            FileWriter writer = null;
+            try {
+                BufferedReader br = new BufferedReader(new FileReader("medico.txt"));
+                String ruta = "medico.txt"; //ruta del archivo que se va a leer
+                writer = new FileWriter(ruta);
+                if (br.readLine() != null) {
+                    writer.write(txtNombreDoctor.getText() + "|" + txtApellidoDoctor.getText() + "|" + cmbEspecialidad.getValue() + "\n");
+                } else {
+                    writer.append(txtNombreDoctor.getText() + "|" + txtApellidoDoctor.getText() + "|" + cmbEspecialidad.getValue() + "\n");
+                }
+                writer.close();
+            } catch (IOException ex) {
+                System.out.println("Archivo no encontrado");
+            }
+
+            txtNombreDoctor.setText("");
+            txtApellidoDoctor.setText("");
+            cmbEspecialidad.setValue("");
+        }
 
     }
 
@@ -88,7 +146,28 @@ public class RegistrosController implements Initializable {
      */
     @FXML
     void guardarPaciente(ActionEvent event) {
-        //Codigo para guardar paciente
+        if (event.getTarget() == btnGuardarPaciente) {
+            FileWriter writer = null;
+            try {
+                BufferedReader br = new BufferedReader(new FileReader("paciente.txt"));
+                String ruta = "paciente.txt"; //ruta del archivo que se va a leer
+                writer = new FileWriter(ruta);
+                if (br.readLine() != null) {
+                    writer.write(txtNombrePaciente.getText() + "|" + txtApellidoPaciente.getText() + "|" + cmbGenero.getValue() + "|" + txtEdad.getText() + "|" + cmbSintomas.getValue() + "\n");
+                } else {
+                    writer.append(txtNombrePaciente.getText() + "|" + txtApellidoPaciente.getText() + "|" + cmbGenero.getValue() + "|" + txtEdad.getText() + "|" + cmbSintomas.getValue() + "\n");
+                }
+                writer.close();
+            } catch (IOException ex) {
+                System.out.println("Archivo no encontrado");
+            }
+
+            txtNombrePaciente.setText("");
+            txtApellidoPaciente.setText("");
+            cmbEspecialidad.setValue("");
+            cmbSintomas.setValue("");
+            txtEdad.setText("");
+        }
         SitemaPrincipalController principal = SitemaPrincipalController.getInstance();
 
     }
@@ -98,10 +177,26 @@ public class RegistrosController implements Initializable {
      */
     @FXML
     void guardarPuesto(ActionEvent event) {
+        if (event.getTarget() == btnGuardarPuesto) {
+            FileWriter writer = null;
+            try {
+                BufferedReader br = new BufferedReader(new FileReader("puesto.txt"));
+                String ruta = "puesto.txt"; //ruta del archivo que se va a leer
+                writer = new FileWriter(ruta);
+                if (br.readLine() != null) {
+                    writer.write(txtNumeroPuesto.getText() + "|" + cmbMedicoresponsable.getValue() + "\n");
+                } else {
+                   writer.write(txtNumeroPuesto.getText() + "|" + cmbMedicoresponsable.getValue() + "\n");
+                }
+                writer.close();
+            } catch (IOException ex) {
+                System.out.println("Archivo no encontrado");
+            }
 
+            txtNumeroPuesto.setText("");
+            cmbMedicoresponsable.setValue("");
+        }
     }
-    
-    
 
     //FUNCIONALIDAD MENU REGISTROS.
     @FXML
